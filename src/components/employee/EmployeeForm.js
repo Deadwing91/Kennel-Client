40e371b64146e71b2786e05359dcc31573b4f90b
nathanpyles.91@gmail.com <nathanpyles.91@gmail.com>
2023-01-20 15:47:23 -0500

    Animals and Employees created and render

--- src/components/employee/EmployeeForm.js
+++ src/components/employee/EmployeeForm.js
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { getAnimals } from "../../managers/animals"
-import { addEmployee } from "../../managers/employees"
+import { addEmployee, getEmployeeById } from "../../managers/employees"
 import { getLocations } from "../../managers/locations"
 import "./Employees.css"
 
@@ -9,28 +9,49 @@ export const EmployeeForm = () => {
   const name = useRef(null)
   const location = useRef(null)
   const animal = useRef(null)
+  const { animalId } = useParams
+  const { employeeId } = useParams()
+  const  address  = useRef(null)
 
   const navigate = useNavigate()
 
   const [animals, setAnimals] = useState([])
   const [locations, setLocations] = useState([])
+  const [employee, setEmployee] = useState({})
+
+  const handleControlledInputChange = (event) => {
+    const newEmployee = Object.assign({}, employee)
+    newEmployee[event.target.name] = event.target.value
+    setEmployee(newEmployee)
+  }
 
   useEffect(() => {
     getAnimals().then(animalsData => setAnimals(animalsData))
     getLocations().then(locationsData => setLocations(locationsData))
   }, [])
 
+  useEffect(() => {
+    if (employeeId) {
+      getEmployeeById(employeeId).then((res) => {
+        setEmployee(res)
+      })
+    }
+  }, [employeeId])
+
   const constructNewEmployee = () => {
-    const locationId = parseInt(location.current.value)
-    const animalId = parseInt(animal.current.value)
+    const locationId = parseInt(employee.location_id)
+    const animalId = parseInt(animal.animal_id)
 
     if (locationId === 0) {
       window.alert("Please select a location")
+    } else if (animalId === 0) {
+      window.alert("Please select an animal")
     } else {
       addEmployee({
-        name: name.current.value,
-        locationId,
-        animalId
+        name: employee?.name,
+        address: employee?.address,
+        locationId: employee?.locationId,
+        animalId: employee?.animalId
       })
         .then(() => navigate("/employees"))
     }
@@ -42,7 +63,21 @@ export const EmployeeForm = () => {
       <fieldset>
         <div className="form-group">
           <label htmlFor="employeeName">Employee name: </label>
-          <input type="text" id="employeeName" ref={name} required autoFocus className="form-control" placeholder="Employee name" />
+          <input type="text" name="employeeName" ref={name} required autoFocus className="form-control" 
+            placeholder="Employee name"
+            defaultValue={employee?.name}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="employeeAddress">Employee address: </label>
+          <input type="text" name="employeeAddress" ref={address} required autoFocus className="form-control" 
+            placeholder="Employee Address"
+            defaultValue={employee?.address}
+            onChange={handleControlledInputChange}
+          />
         </div>
       </fieldset>
       <fieldset>
@@ -61,7 +96,9 @@ export const EmployeeForm = () => {
       <fieldset>
         <div className="form-group">
           <label htmlFor="location">Caretaker for: </label>
-          <select defaultValue="" name="animal" ref={animal} id="employeeAnimal" className="form-control" >
+          <select defaultValue="" name="animal" ref={animal} id="employeeAnimal" className="form-control"
+            value={employee.name}
+            onChange={handleControlledInputChange} >
             <option value="0">Select an animal</option>
             {animals.map(e => (
               <option key={e.id} value={e.id}>
